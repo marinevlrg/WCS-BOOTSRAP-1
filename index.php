da64ce5b4a0c5f419790d89cdba0b7b372e1d7f3
marinevlrg <marine.valorge@gmail.com>
2022-10-24 16:24:32 +0100

    update code

--- index.php
+++ index.php
@@ -11,17 +11,20 @@
     <link rel="stylesheet" href="style.css">
 </head>
 
-<body>
+<body class="d-flex flex-column justify-content-between">
+
+
     <!-- HEADER -->
     <header>
-        <nav class="navbar navbar-dark bg-primary">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Discovering Bootstrap 5.1</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav">
+                <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
+
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
@@ -30,12 +33,12 @@
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Categories
+                                Catégories
                             </a>
                             <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="#">Photos</a></li>
                                 <li><a class="dropdown-item" href="#">Articles</a></li>
-                                <li><a class="dropdown-item" href="#">Projects</a></li>
+                                <li><a class="dropdown-item" href="#">Projets</a></li>
                             </ul>
                         </li>
                     </ul>
@@ -46,59 +49,59 @@
 
     <!-- SECTION ARTICLES -->
 
-    <section>
-        <div class="row">
-            <h1>Journey into Bootstrap classes</h1>
-            <div class="card col-md-3" style="width: 17rem;">
-                <img src="https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Visit Components lake</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn bg-primary">Read more</a>
+    <main>
+        <h1 class="text-center">Journey Around the World</h1>
+        <div class="text-center">
+
+            <div class="row gap-3 justify-content-center">
+                <div class="card col-sm-2 p-0">
+                    <img src="https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk" class="card-img-top" alt="Image d'illustration">
+                    <div class="card-body">
+                        <h5 class="card-title">Visit Components lake</h5>
+                        <p class="card-text w-auto p-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn bg-primary ">Read more</a>
+                    </div>
+                </div>
+                <div class="card col-sm-2 p-0">
+                    <img src="https://i.picsum.photos/id/1035/5854/3903.jpg?hmac=DV0AS2MyjW6ddofvSIU9TVjj1kewfh7J3WEOvflY8TM" class="card-img-top" alt="Image d'illustration">
+                    <div class="card-body">
+                        <h5 class="card-title">Facing Cascade Style Sheet</h5>
+                        <p class="card-text w-auto p-3">Some quick example text.</p>
+                        <a href="#" class="btn bg-primary ">Read more</a>
+                    </div>
+                </div>
+                <div class="card col-sm-2 p-0">
+                    <img src="https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I" class="card-img-top" alt="Image d'illustration">
+                    <div class="card-body">
+                        <h5 class="card-title">Contemplate Form Inputs river</h5>
+                        <p class="card-text w-auto p-3">Make up the bulk of the card's content.</p>
+                        <a href="#" class="btn bg-primary">Read more</a>
+                    </div>
+                </div>
+                <div class="card col-sm-2 p-0">
+                    <img src="https://i.picsum.photos/id/103/2592/1936.jpg?hmac=aC1FT3vX9bCVMIT-KXjHLhP6vImAcsyGCH49vVkAjPQ" class="card-img-top" alt="Image d'illustration">
+                    <div class="card-body">
+                        <h5 class="card-title">Enjoy Responsive Layout land</h5>
+                        <p class="card-text w-auto p-3">Build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn bg-primary">Read more</a>
+                    </div>
                 </div>
             </div>
-            <div id="card2" class="card col-md-3" style="width: 17rem;">
-                <img src="https://i.picsum.photos/id/1035/5854/3903.jpg?hmac=DV0AS2MyjW6ddofvSIU9TVjj1kewfh7J3WEOvflY8TM" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Facing Cascade Style Sheet</h5>
-                    <p class="card-text">Some quick example text.</p>
-                    <a href="#" class="btn bg-primary">Read more</a>
-                </div>
-            </div>
-            <div class="card col-md-3" style="width: 17rem;">
-                <img src="https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Contemplate Form Inputs river</h5>
-                    <p class="card-text">Make up the bulk of the card's content.</p>
-                    <a href="#" class="btn bg-primary">Read more</a>
-                </div>
-            </div>
-            <div class="card col-md-3" style="width: 17rem;">
-                <img src="https://i.picsum.photos/id/103/2592/1936.jpg?hmac=aC1FT3vX9bCVMIT-KXjHLhP6vImAcsyGCH49vVkAjPQ" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Enjoy Responsive Layout land</h5>
-                    <p class="card-text">Build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn bg-primary">Read more</a>
-                </div>
-            </div>
-        </div>
-    </section>
+            </section>
 
-    <!-- SECTION BAS DE PAGE -->
+            <!-- SECTION BAS DE PAGE -->
 
-    <section id="section2">
-        <h2>The css frameworks are very useful</h2>
-        <p>Learning them comes with practice</p>
-    </section>
-
-    <!-- FOOTER -->
-
-    <footer>
-        <p>© 2022 wilders | images <a href="https://picsum.photos/">@picsum</a></p>
-    </footer class="footer bg-primary">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
+            <section id="section2">
+                <h2>The css frameworks are very useful</h2>
+                <p>Learning them comes with practice</p>
+            </section>
 </body>
+
+<!-- FOOTER -->
+<footer>
+    <p>© 2022 wilders | images <a href="https://picsum.photos/">@picsum</a></p>
+</footer class="footer bg-primary">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
 </html>
 
